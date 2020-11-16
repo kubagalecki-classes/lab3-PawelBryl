40b40e72d17ead83077532f2f63dacacf5ed779a
PawelBryl <73634622+PawelBryl@users.noreply.github.com>
2020-11-16 10:33:35 +0100

    transportujFlota implementation

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    if (towar == 0) return 0;
+    
+    unsigned int zagl_count = 0;
+    unsigned int sum = 0;
+    unsigned int przewiezionyTowar;
+    
+    Stocznia stocznia{};
+    while (true) 
+    {
+        Statek* s1 = stocznia();
+        przewiezionyTowar  = s1->transportuj();
+        sum += przewiezionyTowar;
+        if (dynamic_cast<Zaglowiec*>(s1)) zagl_count++;
+        delete s1;
+        if (sum >= towar) return zagl_count;
     return 0;
 }
